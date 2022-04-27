c3947ef5a4389982a9632f81cae610b222fb036e
Alyaksey <alexeykostyukov@yandex.ru>
2022-04-28 00:14:18 +0400

    Initial commit

---
+++ src/main/java/com/example/currencygifservice/CurrencyGifServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencygifservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyGifServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyGifServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/currencygifservice/CurrencyGifServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencygifservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyGifServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
