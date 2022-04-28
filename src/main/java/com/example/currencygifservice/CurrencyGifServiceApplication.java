b5d27b93f86c35d13024730ad187de8ad602636c
Alyaksey <alexeykostyukov@yandex.ru>
2022-04-29 01:19:22 +0400

    Added feign client to dependencies and config parameters to application.properties

--- src/main/java/com/example/currencygifservice/CurrencyGifServiceApplication.java
+++ src/main/java/com/example/currencygifservice/CurrencyGifServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.currencygifservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyGifServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CurrencyGifServiceApplication.class, args);
     }
