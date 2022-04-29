0842bd0b888da49c8748045461b8082668b4a076
Alyaksey <alexeykostyukov@yandex.ru>
2022-04-29 08:05:43 +0400

    Added Error Controller

--- src/main/java/com/example/currencygifservice/services/ExchangeRatesService.java
+++ src/main/java/com/example/currencygifservice/services/ExchangeRatesService.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/example/currencygifservice/utils/NoSuchCurrencyException.java
+++ src/main/java/com/example/currencygifservice/utils/NoSuchCurrencyException.java
@@ -3,7 +3,7 @@ package com.example.currencygifservice.utils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such currency!")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NoSuchCurrencyException extends RuntimeException {
     public NoSuchCurrencyException() {
         super("There's no such currency!");
